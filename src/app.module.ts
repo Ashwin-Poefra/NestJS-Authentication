e43dd73c58a913ccf9c8b621c79ced0af8489415
KasperSky <kaspersky3550879@gmail.com>
2024-11-04 16:59:40 +0900

    add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '1qazxsW@#Edc',
+      username: 'postgres',
+      entities: [],
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
