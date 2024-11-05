5b8bb1c4aee9ff965cf83a8841fee20f1f4aeca1
KasperSky <kaspersky3550879@gmail.com>
2024-11-05 23:11:34 +0900

    feat: add auth entity

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,13 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Auth {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 50})
+  email: string;
+
+  @Column({ type: 'varchar'})
+  password: string;
+}
